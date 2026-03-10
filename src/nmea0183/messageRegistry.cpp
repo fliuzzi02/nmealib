e13c060ca804bb6a6fa2c2b5d21045c99e751bb7
Federico Liuzzi <fliuzzi02@hotmail.it>
2026-03-10 12:26:06 +0100

    [FIX] MessageRegistry to streamline message creation and remove redundant creator methods

--- src/nmea0183/messageRegistry.cpp
+++ src/nmea0183/messageRegistry.cpp
@@ -24,81 +24,33 @@ std::unique_ptr<Message0183> MessageRegistry::create(const std::string& sentence
                                                       std::unique_ptr<Message0183> baseMessage) {
     // TODO: Consider using a map of string to function pointer for better scalability if many sentence types are supported
     if (sentenceType == "DBT") {
-        return createDBT(std::move(baseMessage));
+        return DBT::create(std::move(baseMessage));
     } else if (sentenceType == "GGA") {
-        return createGGA(std::move(baseMessage));
+        return GGA::create(std::move(baseMessage));
     } else if (sentenceType == "GLL") {
-        return createGLL(std::move(baseMessage));
+        return GLL::create(std::move(baseMessage));
     } else if (sentenceType == "GSA") {
-        return createGSA(std::move(baseMessage));
+        return GSA::create(std::move(baseMessage));
     } else if (sentenceType == "HDM") {
-        return createHDM(std::move(baseMessage));
+        return HDM::create(std::move(baseMessage));
     } else if (sentenceType == "MTW") {
-        return createMTW(std::move(baseMessage));
+        return MTW::create(std::move(baseMessage));
     } else if (sentenceType == "MWV") {
-        return createMWV(std::move(baseMessage));
+        return MWV::create(std::move(baseMessage));
     } else if (sentenceType == "RMC") {
-        return createRMC(std::move(baseMessage));
+        return RMC::create(std::move(baseMessage));
     } else if (sentenceType == "VHW") {
-        return createVHW(std::move(baseMessage));
+        return VHW::create(std::move(baseMessage));
     } else if (sentenceType == "VTG") {
-        return createVTG(std::move(baseMessage));
+        return VTG::create(std::move(baseMessage));
     } else if (sentenceType == "VWR") {
-        return createVWR(std::move(baseMessage));
+        return VWR::create(std::move(baseMessage));
     } else if (sentenceType == "ZDA") {
-        return createZDA(std::move(baseMessage));
+        return ZDA::create(std::move(baseMessage));
     } else {
         // Return generic Message0183 if type not registered
         return baseMessage;
     }
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createDBT(std::unique_ptr<Message0183> baseMessage) {
-    return DBT::create(std::move(baseMessage));
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createGGA(std::unique_ptr<Message0183> baseMessage) {
-    return GGA::create(std::move(baseMessage));
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createGLL(std::unique_ptr<Message0183> baseMessage) {
-    return GLL::create(std::move(baseMessage));
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createGSA(std::unique_ptr<Message0183> baseMessage) {
-    return GSA::create(std::move(baseMessage));
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createHDM(std::unique_ptr<Message0183> baseMessage) {
-    return HDM::create(std::move(baseMessage));
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createMTW(std::unique_ptr<Message0183> baseMessage) {
-    return MTW::create(std::move(baseMessage));
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createMWV(std::unique_ptr<Message0183> baseMessage) {
-    return MWV::create(std::move(baseMessage));
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createRMC(std::unique_ptr<Message0183> baseMessage) {
-    return RMC::create(std::move(baseMessage));
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createVHW(std::unique_ptr<Message0183> baseMessage) {
-    return VHW::create(std::move(baseMessage));
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createVTG(std::unique_ptr<Message0183> baseMessage) {
-    return VTG::create(std::move(baseMessage));
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createVWR(std::unique_ptr<Message0183> baseMessage) {
-    return VWR::create(std::move(baseMessage));
-}
-
-std::unique_ptr<Message0183> MessageRegistry::createZDA(std::unique_ptr<Message0183> baseMessage) {
-    return ZDA::create(std::move(baseMessage));
 }
 
 } // namespace nmea0183
